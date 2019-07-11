39f398253d2e8618ea9f309d456354f89f888110
Jack.A.Black <lv4399@126.com>
2019-07-11 16:31:49 +0800

    features required: 1. Sound alarm
    implemented function.
    1. Auto capture photo @ 320*240
    2. implement class Notify (API ready)

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,6 +8,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
@@ -25,6 +26,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.List;
 
 
 /**
@@ -49,6 +51,7 @@ class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
         try {
+            setCameraParams(320,240);
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
             Log.d("Success", "surfaceCreated: preview started");
@@ -84,6 +87,67 @@ class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
         } catch (Exception e) {
             Log.d("Preview start Err:", "Error starting camera preview: " + e.getMessage());
         }
+    }
+
+    private void setCameraParams(int width, int height) {
+        Log.i("info", "setCameraParams  width=" + width + "  height=" + height);
+        Camera.Parameters parameters = mCamera.getParameters();
+        /*************************** 获取摄像头支持的PictureSize列表********************/
+        List<Camera.Size> pictureSizeList = parameters.getSupportedPictureSizes();
+        for (Camera.Size size : pictureSizeList) {
+            Log.i("info", "摄像头支持的分辨率：" + " size.width=" + size.width + "  size.height=" + size.height);
+        }
+        Camera.Size picSize = getBestSupportedSize(pictureSizeList, ((float) height / width));//从列表中选取合适的分辨率
+        if (null == picSize) {
+            picSize = parameters.getPictureSize();
+        }
+        Log.e("info", "我们选择的摄像头分辨率：" + "picSize.width=" + picSize.width + "  picSize.height=" + picSize.height);
+        // 根据选出的PictureSize重新设置SurfaceView大小
+        parameters.setPictureSize(picSize.width, picSize.height);
+        /*************************** 获取摄像头支持的PreviewSize列表********************/
+        List<Camera.Size> previewSizeList = parameters.getSupportedPreviewSizes();
+        for (Camera.Size size : previewSizeList) {
+            Log.i("info", "摄像支持可预览的分辨率：" + " size.width=" + size.width + "  size.height=" + size.height);
+        }
+        Camera.Size preSize = getBestSupportedSize(previewSizeList, ((float) height) / width);
+        if (null != preSize) {
+            Log.e("info", "我们选择的预览分辨率：" + "preSize.width=" + preSize.width + "  preSize.height=" + preSize.height);
+            parameters.setPreviewSize(preSize.width, preSize.height);
+        }
+
+        mCamera.cancelAutoFocus();
+        mCamera.setParameters(parameters);
+    }
+
+    private Camera.Size getBestSupportedSize(List<Camera.Size> sizes, float screenRatio) {
+        Camera.Size largestSize = null;
+        int DEFAULT_PHOTO_WIDTH = 360;
+        int DEFAULT_PHOTO_HEIGHT = 480;
+        int largestArea = 0;
+        for (Camera.Size size : sizes) {
+            if ((float) size.height / (float) size.width == screenRatio) {
+                if (size.width == DEFAULT_PHOTO_WIDTH && size.height == DEFAULT_PHOTO_HEIGHT) {
+                    // 包含特定的尺寸，直接取该尺寸
+                    largestSize = size;
+                    break;
+                } else if (size.height == DEFAULT_PHOTO_HEIGHT || size.width == DEFAULT_PHOTO_WIDTH) {
+                    largestSize = size;
+                    break;
+                }
+                int area = size.height + size.width;
+                if (area > largestArea) {//找出最大的合适尺寸
+                    largestArea = area;
+                    largestSize = size;
+                }
+            } else if (size.height == DEFAULT_PHOTO_HEIGHT || size.width == DEFAULT_PHOTO_WIDTH) {
+                largestSize = size;
+                break;
+            }
+        }
+        if (largestSize == null) {
+            largestSize = sizes.get(sizes.size() - 1);
+        }
+        return largestSize;
     }
 }
 
